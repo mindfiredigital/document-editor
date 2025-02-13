6c7c972306d4ffd2df5a7259b7e03967e740b8e9
anandmindfire <anand.k@mindfiresolutions.com>
2025-02-13 16:34:20 +0530

    feat(docs):  add docs for react

---
+++ documentation/.docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('D:\\document-editor\\documentation\\node_modules\\infima\\dist\\css\\default\\default.css'),
+  require('D:\\document-editor\\documentation\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
+  require('D:\\document-editor\\documentation\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress'),
+  require('D:\\document-editor\\documentation\\src\\css\\custom.css'),
+];
