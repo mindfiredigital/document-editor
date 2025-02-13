12b6f3f29e8bc875506f9a63ff05793b45a9ca74
anandmindfire <anand.k@mindfiresolutions.com>
2025-02-13 16:43:42 +0530

    feat(docs):  add docs docusaurus

--- .github/changeset-autogenerate.mjs
+++ .github/changeset-autogenerate.mjs
@@ -5,7 +5,7 @@ import fs from 'fs';
 const commitMessage = execSync('git log -1 --format=%s').toString().trim();
 
 // Define valid scopes
-const validScopes = ['core', 'react', 'webcomponent','angular','release','docs'];
+const validScopes = ['core', 'react', 'webcomponent','angular'];
 
 // Define regex patterns
 const commitPatterns = {
